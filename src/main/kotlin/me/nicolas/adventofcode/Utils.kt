35e4a6eb371cc221f36fe2d4f7c4ae3e73f34999
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-12-06 10:41:09 +0100

    Day6

---
+++ src/main/kotlin/me/nicolas/adventofcode/Utils.kt
@@ -0,0 +1,9 @@
+package me.nicolas.adventofcode
+
+import java.io.File
+import java.nio.file.Paths
+
+fun readFileDirectlyAsText(fileName: String): String {
+    val path = Paths.get("src/main/resources/me/nicolas/adventofcode").toAbsolutePath().toString()
+    return File("$path/$fileName").readText(Charsets.UTF_8)
+}
---
+++ src/main/kotlin/me/nicolas/adventofcode/year2020/Day6.kt
@@ -0,0 +1,37 @@
+package me.nicolas.adventofcode.year2020
+
+import me.nicolas.adventofcode.readFileDirectlyAsText
+
+// --- Day 6: Custom Customs ---
+// https://adventofcode.com/2020/day/6
+fun main() {
+
+    val trainingData = readFileDirectlyAsText("/year2020/day6/training.txt")
+    val data = readFileDirectlyAsText("/year2020/day6/data.txt")
+
+    val answers = data.split("\n\n")
+
+    // Part One
+    partOne(answers)
+
+    // Part Two
+    partTwo(answers)
+}
+
+private fun partOne(answers: List<String>) {
+    val count = answers.sumOf { str -> str.toSet().count { char -> char != '\n' } }
+    println("Part one = $count")
+}
+
+private fun partTwo(answers: List<String>) {
+    val count = answers.sumOf { answer ->
+        val nbLines = answer.count { char -> char == '\n' } + 1
+        answer.replace("\n", "")
+            .groupBy { char -> char }
+            .filterValues { chars -> chars.size == nbLines }
+            .count()
+    }
+    println("Part two = $count")
+}
+
+
