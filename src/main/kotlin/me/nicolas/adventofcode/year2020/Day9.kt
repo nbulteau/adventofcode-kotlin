00d117f420e8a3cc112df1dd374504fa654e6822
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-12-09 07:46:34 +0100

    day9

---
+++ src/main/kotlin/me/nicolas/adventofcode/year2020/Day9.kt
@@ -0,0 +1,61 @@
+package me.nicolas.adventofcode.year2020
+
+import me.nicolas.adventofcode.readFileDirectlyAsText
+
+// --- Day 9: Encoding Error ---
+// https://adventofcode.com/2020/day/9
+fun main() {
+
+    val training = readFileDirectlyAsText("/year2020/day9/training.txt")
+    val data = readFileDirectlyAsText("/year2020/day9/data.txt")
+
+    val numbers = data.split("\n").map { str -> str.toLong() }
+
+    // Part One
+    val sum = partOne(numbers, preamble = 25)
+
+    // Part Two
+    partTwo(numbers, sum)
+}
+
+private fun partOne(numbers: List<Long>, preamble: Int): Long {
+    var index = 0
+    var wrongNumber: Long? = null
+
+    while (index < numbers.size && wrongNumber == null) {
+        val previous = numbers.subList(index, index + preamble)
+        val nextNumbers = numbers.subList(index + preamble, numbers.size)
+
+        val filter = previous.filter { nextNumbers[0] - it in previous && (nextNumbers[0] - it) * 2 != it }
+        if (filter.isEmpty()) {
+            wrongNumber = nextNumbers[0]
+        }
+        index++
+    }
+
+    println("Part one = $wrongNumber")
+
+    return wrongNumber!!
+}
+
+
+private fun partTwo(numbers: List<Long>, sum: Long) {
+    val list = numbers.subList(0, numbers.indexOf(sum))
+
+    var index = 0
+    do {
+        var k = index + 1
+        do {
+            val contiguousRange = list.subList(index, k)
+            if (contiguousRange.sum() == sum) {
+                println("Part two = ${contiguousRange.minOf { it } + contiguousRange.maxOf { it }}")
+                return // done -> exit partTwo
+            }
+
+            k++
+        } while (k < list.size)
+
+        index++
+    } while (index < list.size)
+}
+
