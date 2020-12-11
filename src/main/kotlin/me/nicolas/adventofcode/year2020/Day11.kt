25040cc3913d521253147d5f7858743f42f0c6d3
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-12-11 14:30:31 +0100

    day11

---
+++ src/main/kotlin/me/nicolas/adventofcode/year2020/Day11.kt
@@ -0,0 +1,181 @@
+package me.nicolas.adventofcode.year2020
+
+import me.nicolas.adventofcode.readFileDirectlyAsText
+
+typealias Layout = List<String>
+
+// --- Day 11: Seating System ---
+// https://adventofcode.com/2020/day/11
+fun main() {
+
+    println("--- Day 11: Seating System ---")
+    println()
+
+    val training = readFileDirectlyAsText("/year2020/day11/training.txt")
+    val data = readFileDirectlyAsText("/year2020/day11/data.txt")
+
+    val layout: Layout = training.split("\n")
+
+    // Part One
+    partOne(layout)
+
+    // Part Two
+    partTwo(layout)
+}
+
+private fun partOne(layout: Layout) {
+
+    var newLayout = layout
+    var previousLayout: List<String>
+    do {
+        previousLayout = newLayout
+        newLayout = previousLayout.processLayoutPartOne()
+
+        // newLayout.displayLayout()
+    } while (newLayout != previousLayout)
+
+    println("Part one = ${newLayout.count('#')}")
+}
+
+private fun partTwo(layout: Layout) {
+
+    var newLayout = layout
+    var previousLayout: List<String>
+    do {
+        previousLayout = newLayout
+        newLayout = previousLayout.processLayoutPartTwo()
+
+        //newLayout.displayLayout()
+    } while (newLayout != previousLayout)
+
+    println("Part two = ${newLayout.count('#')}")
+}
+
+fun Layout.processLayoutPartOne(): Layout {
+    val height = this.size - 1
+    val width = this[0].length - 1
+    val newLayout = mutableListOf<String>()
+
+    for (y in 0..height) {
+        var newString = ""
+
+        for (x in 0..width) {
+            when (this[y][x]) {
+                '.' -> newString += "."
+                'L' -> {
+                    newString += if (this.lookAround(x, y) == 0) {
+                        "#"
+                    } else {
+                        "L"
+                    }
+                }
+                '#' -> {
+                    newString += if (this.lookAround(x, y) >= 4) {
+                        "L"
+                    } else {
+                        "#"
+                    }
+                }
+            }
+        }
+        newLayout += newString
+    }
+    return newLayout
+}
+
+fun Layout.processLayoutPartTwo(): Layout {
+    val height = this.size - 1
+    val width = this[0].length - 1
+    val newLayout = mutableListOf<String>()
+
+    for (y in 0..height) {
+        var newString = ""
+
+        for (x in 0..width) {
+            when (this[y][x]) {
+                '.' -> newString += "."
+                'L' -> {
+                    newString += if (this.lookInDirection(x, y) == 0) {
+                        "#"
+                    } else {
+                        "L"
+                    }
+                }
+                '#' -> {
+                    newString += if (this.lookInDirection(x, y) >= 5) {
+                        "L"
+                    } else {
+                        "#"
+                    }
+                }
+            }
+        }
+        newLayout += newString
+    }
+    return newLayout
+}
+
+private fun Layout.displayLayout() {
+    println()
+    this.forEach { string -> println(string) }
+}
+
+private fun Layout.count(charToCount: Char) =
+    this.sumOf { string -> string.count { char -> char == charToCount } }
+
+fun Layout.lookAround(x: Int, y: Int) =
+    Direction.values().sumBy { direction -> this.lookAround(x, y, direction) }
+
+fun Layout.lookAround(x: Int, y: Int, direction: Direction): Int {
+
+    val height = this.size - 1
+    val width = this[0].length - 1
+
+    var nextX = x + direction.dx
+    var nextY = y + direction.dy
+
+    if (nextX in 0..width && nextY in 0..height) {
+        if (this[nextY][nextX] == '#') {
+            return 1
+        }
+    }
+
+    return 0
+}
+
+fun Layout.lookInDirection(x: Int, y: Int) =
+    Direction.values().sumBy { direction -> this.lookInDirection(x, y, direction) }
+
+fun Layout.lookInDirection(x: Int, y: Int, direction: Direction): Int {
+
+    val height = this.size - 1
+    val width = this[0].length - 1
+
+    var nextX = x + direction.dx
+    var nextY = y + direction.dy
+
+    while (nextX in 0..width && nextY in 0..height) {
+        if (this[nextY][nextX] == 'L') {
+            return 0
+        }
+        if (this[nextY][nextX] == '#') {
+            return 1
+        }
+
+        nextX += direction.dx
+        nextY += direction.dy
+    }
+
+    return 0
+}
+
+enum class Direction(var dx: Int, var dy: Int) {
+    NORTH(0, -1),
+    NORTH_EAST(1, -1),
+    EAST(1, 0),
+    SOUTH_EAST(1, 1),
+    SOUTH(0, 1),
+    SOUTH_WEST(-1, 1),
+    WEST(-1, 0),
+    NORTH_WEST(-1, -1);
+}
---
+++ src/test/kotlin/me/nicolas/adventofcode/year2020/Day11KtTest.kt
@@ -0,0 +1,139 @@
+package me.nicolas.adventofcode.year2020
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Day11KtTest {
+
+
+    @Test
+    fun lookAroundInDirection3() {
+        val layout = """
+            .......#.
+            ...#.....
+            .#.......
+            .........
+            ..#L....#
+            ....#....
+            .........
+            #........
+            ...#.....
+        """.trimIndent().split("\n")
+
+        var nbSeats = layout.lookInDirection(3, 4, Direction.EAST)
+        assertEquals(1, nbSeats)
+
+        nbSeats = layout.lookInDirection(3, 4)
+        assertEquals(8, nbSeats)
+    }
+
+    @Test
+    fun lookAroundInDirection2() {
+        val layout = """
+            .............
+            .L.L.#.#.#.#.
+            .............
+        """.trimIndent().split("\n")
+
+        var nbSeats = layout.lookInDirection(1, 1, Direction.EAST)
+        assertEquals(1, nbSeats)
+
+        nbSeats = layout.lookInDirection(1, 1)
+        assertEquals(1, nbSeats)
+    }
+
+    @Test
+    fun lookAroundInDirection1() {
+        val layout = """
+            .##.##.
+            #.#.#.#
+            ##...##
+            ...L...
+            ##...##
+            #.#.#.#
+            .##.##.
+        """.trimIndent().split("\n")
+
+        var nbSeats = layout.lookInDirection(3, 3, Direction.EAST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.NORTH)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.WEST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.SOUTH)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.SOUTH_EAST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.SOUTH_WEST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.NORTH_EAST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3, Direction.NORTH_WEST)
+        assertEquals(0, nbSeats)
+        nbSeats = layout.lookInDirection(3, 3)
+        assertEquals(0, nbSeats)
+    }
+
+    @Test
+    fun processLayoutPartTwo() {
+        val layout = """
+            L.LL.LL.LL
+            LLLLLLL.LL
+            L.L.L..L..
+            LLLL.LL.LL
+            L.LL.LL.LL
+            L.LLLLL.LL
+            ..L.L.....
+            LLLLLLLLLL
+            L.LLLLLL.L
+            L.LLLLL.LL
+        """.trimIndent().split("\n")
+
+        val result1 = layout.processLayoutPartTwo()
+        val expected1 = """
+            #.##.##.##
+            #######.##
+            #.#.#..#..
+            ####.##.##
+            #.##.##.##
+            #.#####.##
+            ..#.#.....
+            ##########
+            #.######.#
+            #.#####.##
+        """.trimIndent().split("\n")
+        assertEquals(expected1, result1)
+
+        val result2 = result1.processLayoutPartTwo()
+        val expected2 = """
+            #.LL.LL.L#
+            #LLLLLL.LL
+            L.L.L..L..
+            LLLL.LL.LL
+            L.LL.LL.LL
+            L.LLLLL.LL
+            ..L.L.....
+            LLLLLLLLL#
+            #.LLLLLL.L
+            #.LLLLL.L#
+        """.trimIndent().split("\n")
+        assertEquals(expected2, result2)
+
+        val result3 = result2.processLayoutPartTwo()
+        val expected3 = """
+            #.L#.##.L#
+            #L#####.LL
+            L.#.#..#..
+            ##L#.##.##
+            #.##.#L.##
+            #.#####.#L
+            ..#.#.....
+            LLL####LL#
+            #.L#####.L
+            #.L####.L#
+        """.trimIndent().split("\n")
+
+        assertEquals(expected3, result3)
+
+    }
+}
