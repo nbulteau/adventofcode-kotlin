6b33f5e5cd50b3ab70d93e8aaa011192996d050f
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-12-12 13:52:36 +0100

    day12 - 2020

---
+++ src/main/kotlin/me/nicolas/adventofcode/year2020/Day12.kt
@@ -0,0 +1,159 @@
+package me.nicolas.adventofcode.year2020
+
+import me.nicolas.adventofcode.readFileDirectlyAsText
+import kotlin.math.abs
+
+// --- Day 12: Rain Risk ---
+// https://adventofcode.com/2020/day/12
+fun main() {
+
+
+    println("--- Day 12: Rain Risk ---")
+    println()
+
+    val training = readFileDirectlyAsText("/year2020/day12/training.txt")
+    val data = readFileDirectlyAsText("/year2020/day12/data.txt")
+
+    val instructions =
+        data.split("\n").map { line -> Day12.Instruction(line.elementAt(0), line.substring(1).toInt()) }
+
+    // Part One
+    Day12().partOne(instructions)
+
+    // Part Two
+    Day12().partTwo(instructions)
+}
+
+class Day12 {
+    data class Instruction(val action: Char, val value: Int)
+
+    class TaxicabGeometry(startDirection: Direction = Direction.EAST) {
+
+        var currentDirection = startDirection
+        private var eastWest = 0
+        private var northSouth = 0
+
+        fun move(instructions: List<Instruction>) {
+            instructions.forEach { instruction -> move(instruction) }
+        }
+
+        fun move(instruction: Instruction) {
+            when (instruction.action) {
+                'N', 'E', 'S', 'W' -> move(Direction.byLabel(instruction.action), instruction.value)
+                'L' -> currentDirection = currentDirection.getNextDirection(360 - instruction.value)
+                'R' -> currentDirection = currentDirection.getNextDirection(instruction.value)
+                'F' -> move(currentDirection, instruction.value)
+            }
+        }
+
+        private fun move(direction: Direction, value: Int) {
+            eastWest += value * direction.dx
+            northSouth += value * direction.dy
+        }
+
+        val path: Int
+            get() = abs(eastWest) + abs(northSouth)
+    }
+
+    fun partOne(instructions: List<Instruction>) {
+
+        val taxicabGeometry = TaxicabGeometry()
+        taxicabGeometry.move(instructions)
+
+        println("Part one = ${taxicabGeometry.path}")
+    }
+
+    class TaxicabGeometryWithWayPoint(startWaypointEastWest: Int = -10, startWaypointNorthSouth: Int = 1) {
+
+        var waypointEastWest = startWaypointEastWest
+        var waypointNorthSouth = startWaypointNorthSouth
+        private var eastWest = 0
+        private var northSouth = 0
+
+        fun move(instructions: List<Instruction>) {
+            instructions.forEach { instruction -> move(instruction) }
+        }
+
+        fun move(instruction: Instruction) {
+            when (instruction.action) {
+                'N', 'E', 'S', 'W' -> moveWaypoint(Direction.byLabel(instruction.action), instruction.value)
+                'L' -> {
+                    when (instruction.value) {
+                        90 -> {
+                            val previousWaypointEastWest = waypointEastWest
+                            waypointEastWest = waypointNorthSouth
+                            waypointNorthSouth = 0 - previousWaypointEastWest
+                        }
+                        180 -> {
+                            waypointEastWest = 0 - waypointEastWest
+                            waypointNorthSouth = 0 - waypointNorthSouth
+                        }
+                        270 -> {
+                            val previousWaypointEastWest = waypointEastWest
+                            waypointEastWest = 0 - waypointNorthSouth
+                            waypointNorthSouth = previousWaypointEastWest
+                        }
+                    }
+                }
+                'R' -> {
+                    when (instruction.value) {
+                        90 -> {
+                            val previousWaypointEastWest = waypointEastWest
+                            waypointEastWest = 0 - waypointNorthSouth
+                            waypointNorthSouth = previousWaypointEastWest
+                        }
+                        180 -> {
+                            waypointEastWest = 0 - waypointEastWest
+                            waypointNorthSouth = 0 - waypointNorthSouth
+                        }
+                        270 -> {
+                            val previousWaypointEastWest = waypointEastWest
+                            waypointEastWest = waypointNorthSouth
+                            waypointNorthSouth = 0 - previousWaypointEastWest
+                        }
+                    }
+                }
+                'F' -> {
+                    eastWest += waypointEastWest * instruction.value
+                    northSouth += waypointNorthSouth * instruction.value
+                }
+            }
+        }
+
+        private fun moveWaypoint(direction: Direction, value: Int) {
+            waypointEastWest += value * direction.dx
+            waypointNorthSouth += value * direction.dy
+        }
+
+        private fun rotateWaypoint(direction: Direction, value: Int) {
+            waypointEastWest += value * direction.dx
+            waypointNorthSouth += value * direction.dy
+        }
+
+        val path: Int
+            get() = abs(eastWest) + abs(northSouth)
+    }
+
+    fun partTwo(instructions: List<Instruction>) {
+
+        val taxicabGeometry = TaxicabGeometryWithWayPoint()
+        taxicabGeometry.move(instructions)
+
+        println("Part two = ${taxicabGeometry.path}")
+    }
+
+    enum class Direction(val label: Char, val dx: Int, val dy: Int) {
+        NORTH('N', 0, 1),
+        EAST('E', -1, 0),
+        SOUTH('S', 0, -1),
+        WEST('W', 1, 0);
+
+        companion object {
+            fun byLabel(label: Char) = values().first { direction -> label == direction.label }
+        }
+
+        fun getNextDirection(value: Int): Direction {
+            return values()[(ordinal + (value / 90)) % 4]
+        }
+    }
+}
---
+++ src/test/kotlin/me/nicolas/adventofcode/year2020/Day12Test.kt
@@ -0,0 +1,120 @@
+package me.nicolas.adventofcode.year2020
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day12Test {
+
+    @Test
+    fun getNextDirection() {
+
+        assertEquals(Day12.Direction.EAST, Day12.Direction.EAST.getNextDirection(0))
+        assertEquals(Day12.Direction.SOUTH, Day12.Direction.EAST.getNextDirection(90))
+        assertEquals(Day12.Direction.WEST, Day12.Direction.EAST.getNextDirection(180))
+        assertEquals(Day12.Direction.NORTH, Day12.Direction.EAST.getNextDirection(270))
+        assertEquals(Day12.Direction.NORTH, Day12.Direction.NORTH.getNextDirection(0))
+        assertEquals(Day12.Direction.EAST, Day12.Direction.NORTH.getNextDirection(90))
+        assertEquals(Day12.Direction.SOUTH, Day12.Direction.NORTH.getNextDirection(180))
+        assertEquals(Day12.Direction.WEST, Day12.Direction.NORTH.getNextDirection(270))
+        assertEquals(Day12.Direction.WEST, Day12.Direction.WEST.getNextDirection(0))
+        assertEquals(Day12.Direction.NORTH, Day12.Direction.WEST.getNextDirection(90))
+        assertEquals(Day12.Direction.EAST, Day12.Direction.WEST.getNextDirection(180))
+        assertEquals(Day12.Direction.SOUTH, Day12.Direction.WEST.getNextDirection(270))
+        assertEquals(Day12.Direction.SOUTH, Day12.Direction.SOUTH.getNextDirection(0))
+        assertEquals(Day12.Direction.WEST, Day12.Direction.SOUTH.getNextDirection(90))
+        assertEquals(Day12.Direction.NORTH, Day12.Direction.SOUTH.getNextDirection(180))
+        assertEquals(Day12.Direction.EAST, Day12.Direction.SOUTH.getNextDirection(270))
+    }
+
+    @Test
+    fun `taxicabGeometry1 sample`() {
+
+        val taxicabGeometry = Day12.TaxicabGeometry()
+
+        taxicabGeometry.move(Day12.Instruction('F', 10))
+        assertEquals(10, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('N', 3))
+        assertEquals(13, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('F', 7))
+        assertEquals(20, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('R', 90))
+        assertEquals(20, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('F', 11))
+        assertEquals(25, taxicabGeometry.path)
+    }
+
+    @Test
+    fun `taxicabGeometry1 rotate`() {
+
+        val taxicabGeometry = Day12.TaxicabGeometry()
+
+        taxicabGeometry.move(Day12.Instruction('R', 0))
+        assertEquals(Day12.Direction.EAST, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('R', 90))
+        assertEquals(Day12.Direction.SOUTH, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('R', 180))
+        assertEquals(Day12.Direction.NORTH, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('R', 270))
+        assertEquals(Day12.Direction.WEST, taxicabGeometry.currentDirection)
+
+        taxicabGeometry.move(Day12.Instruction('L', 0))
+        assertEquals(Day12.Direction.WEST, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('L', 90))
+        assertEquals(Day12.Direction.SOUTH, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('L', 180))
+        assertEquals(Day12.Direction.NORTH, taxicabGeometry.currentDirection)
+        taxicabGeometry.move(Day12.Instruction('L', 270))
+        assertEquals(Day12.Direction.EAST, taxicabGeometry.currentDirection)
+    }
+
+    @Test
+    fun `taxicabGeometry2 sample`() {
+
+        val taxicabGeometry = Day12.TaxicabGeometryWithWayPoint()
+
+        taxicabGeometry.move(Day12.Instruction('F', 10))
+        assertEquals(110, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('N', 3))
+        assertEquals(110, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('F', 7))
+        assertEquals(208, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('R', 90))
+        assertEquals(208, taxicabGeometry.path)
+        taxicabGeometry.move(Day12.Instruction('F', 11))
+        assertEquals(286, taxicabGeometry.path)
+    }
+
+    @Test
+    fun `taxicabGeometry2 rotate`() {
+
+        val taxicabGeometry =
+            Day12.TaxicabGeometryWithWayPoint(startWaypointEastWest = -10, startWaypointNorthSouth = 1)
+
+        taxicabGeometry.move(Day12.Instruction('R', 0))
+        assertEquals(-10, taxicabGeometry.waypointEastWest)
+        assertEquals(1, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('R', 90))
+        assertEquals(-1, taxicabGeometry.waypointEastWest)
+        assertEquals(-10, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('R', 180))
+        assertEquals(1, taxicabGeometry.waypointEastWest)
+        assertEquals(10, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('R', 270))
+        assertEquals(10, taxicabGeometry.waypointEastWest)
+        assertEquals(-1, taxicabGeometry.waypointNorthSouth)
+
+        taxicabGeometry.move(Day12.Instruction('L', 0))
+        assertEquals(10, taxicabGeometry.waypointEastWest)
+        assertEquals(-1, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('L', 90))
+        assertEquals(-1, taxicabGeometry.waypointEastWest)
+        assertEquals(-10, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('L', 180))
+        assertEquals(1, taxicabGeometry.waypointEastWest)
+        assertEquals(10, taxicabGeometry.waypointNorthSouth)
+        taxicabGeometry.move(Day12.Instruction('L', 270))
+        assertEquals(-10, taxicabGeometry.waypointEastWest)
+        assertEquals(1, taxicabGeometry.waypointNorthSouth)
+    }
+
+}
