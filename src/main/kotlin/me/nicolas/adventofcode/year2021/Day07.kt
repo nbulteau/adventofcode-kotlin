f3278f4ca502998af236751f057b1542d1de9bce
Nicolas Bulteau <nicolas.bulteau@orange.com>
2021-12-07 09:54:50 +0100

    day 7 2021

--- src/main/kotlin/me/nicolas/adventofcode/year2021/Day07.kt
+++ src/main/kotlin/me/nicolas/adventofcode/year2021/Day07.kt
@@ -1,17 +1,36 @@
 package me.nicolas.adventofcode.year2021
 
+import me.nicolas.adventofcode.prettyPrint
 import me.nicolas.adventofcode.readFileDirectlyAsText
+import kotlin.math.abs
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 // https://adventofcode.com/2021/day/7
+@OptIn(ExperimentalTime::class)
 fun main() {
 
     val training = readFileDirectlyAsText("/year2021/day07/training.txt")
     val data = readFileDirectlyAsText("/year2021/day07/data.txt")
+    val positions = data.split(",").map { it.toInt() }
 
+    prettyPrint(
+        message = "Part one answer = ",
+        measureTimedValue { Day07().solve(positions) { position -> position } })
+
+    prettyPrint(
+        message = "Part two answer = ",
+        measureTimedValue { Day07().solve(positions) { position -> (1..position).sumOf { it } } })
 }
 
-private class Day07s {
+private class Day07 {
 
+    fun solve(positions: List<Int>, costFunction: (position: Int) -> Int): Int {
+
+        val max = positions.maxOf { it }
+        val moves = (0..max).associateWith { positions.sumOf { position -> costFunction(abs(position - it)) } }
+        return moves.values.minOf { it }
+    }
 }
 
 
