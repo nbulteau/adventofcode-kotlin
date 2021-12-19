85154f6c65d50471d39b3e0aacedda4c998052a5
Nicolas Bulteau <nicolas.bulteau@orange.com>
2021-12-19 22:36:27 +0100

    day 18 2021

--- src/main/kotlin/me/nicolas/adventofcode/year2021/Day18.kt
+++ src/main/kotlin/me/nicolas/adventofcode/year2021/Day18.kt
@@ -1,7 +1,10 @@
 package me.nicolas.adventofcode.year2021
 
+import me.nicolas.adventofcode.prettyPrint
 import me.nicolas.adventofcode.readFileDirectlyAsText
+import kotlin.math.max
 import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 
 // https://adventofcode.com/2021/day/18
@@ -11,8 +14,183 @@ fun main() {
     val training = readFileDirectlyAsText("/year2021/day18/training.txt")
     val data = readFileDirectlyAsText("/year2021/day18/data.txt")
 
+    val lines = data.split("\n")
+
+    prettyPrint(
+        message = "Part one answer",
+        measureTimedValue { Day18().partOne(lines) })
+
+    prettyPrint(
+        message = "Part two answer",
+        measureTimedValue { Day18().partTwo(lines) })
 }
 
-private class Day18 {
+class Day18 {
+
+    fun partOne(lines: List<String>): Int {
+        val sNumbers = lines.map { line -> parseLine(line) }
+        val sNumber = sNumbers.reduce { acc, sNum -> add(acc, sNum) }
+
+        return sNumber.magnitude()
+    }
+
+    fun partTwo(lines: List<String>): Int {
+        val sNumbers = lines.map { line -> parseLine(line) }
+
+        var magnitudeMax = 0
+        for (i in sNumbers.indices) {
+            for (j in sNumbers.indices) {
+                if (i != j) {
+                    magnitudeMax = max(magnitudeMax, add(sNumbers[i], sNumbers[j]).magnitude())
+                }
+            }
+        }
+
+        return magnitudeMax
+    }
+
+    sealed class SnailfishNumber {
+
+        private fun findPairToExplode(nestedLevel: Int): Pair? {
+            if (nestedLevel == 0)
+                return this as? Pair?
+            if (this is Pair) {
+                left.findPairToExplode(nestedLevel - 1)?.let { return it }
+                right.findPairToExplode(nestedLevel - 1)?.let { return it }
+            }
+            return null
+        }
+
+        fun reduce(): SnailfishNumber {
+            var snailfishNumber = this
+            do {
+                val pairToExplode = snailfishNumber.findPairToExplode(4)
+                val regularNumberToSplit = snailfishNumber.findRegularNumberToSplit()
+
+                if (pairToExplode != null) {
+                    snailfishNumber = snailfishNumber.explode(pairToExplode)
+                } else if (regularNumberToSplit != null) {
+                    snailfishNumber = snailfishNumber.split(regularNumberToSplit)
+                }
+            } while ((pairToExplode != null) || (regularNumberToSplit != null))
+
+            return snailfishNumber
+        }
+
+        private fun explode(pairToExplode: Pair): SnailfishNumber {
+            val explodeOperations =
+                mutableMapOf<SnailfishNumber, SnailfishNumber>(pairToExplode to RegularNumber(0))
+
+            val all = traverse(pairToExplode)
+            val index = all.indexOf(pairToExplode)
+            // look left
+            val leftRegularNumber = all.getOrNull(index - 1) as? RegularNumber
+            leftRegularNumber?.let {
+                val left = pairToExplode.left as RegularNumber
+                explodeOperations[it] = RegularNumber(it.value + left.value)
+            }
+            // look right
+            val rightRegularNumber = all.getOrNull(index + 1) as? RegularNumber
+            rightRegularNumber?.let {
+                val right = pairToExplode.right as RegularNumber
+                explodeOperations[it] = RegularNumber(it.value + right.value)
+            }
+
+            return this.replace(explodeOperations)
+        }
+
+        private fun split(regularNumberToSplit: RegularNumber): SnailfishNumber {
+            // split regular number : the left element of the pair should be the regular number divided by two and rounded down,
+            // while the right element of the pair should be the regular number divided by two and rounded up.
+            val splitOperations = mapOf<SnailfishNumber, SnailfishNumber>(
+                regularNumberToSplit to Pair(
+                    RegularNumber(regularNumberToSplit.value / 2),
+                    RegularNumber((regularNumberToSplit.value + 1) / 2)
+                )
+            )
+
+            return this.replace(splitOperations)
+        }
+
+        abstract fun replace(operations: Map<SnailfishNumber, SnailfishNumber>): SnailfishNumber
+
+        abstract fun findRegularNumberToSplit(): RegularNumber?
+
+        abstract fun traverse(keep: Pair): List<SnailfishNumber>
+
+        abstract fun magnitude(): Int
+    }
+
+    class RegularNumber(val value: Int) : SnailfishNumber() {
+        override fun replace(operations: Map<SnailfishNumber, SnailfishNumber>): SnailfishNumber {
+            operations[this]?.let { return it }
+            return this
+        }
+
+        override fun findRegularNumberToSplit(): RegularNumber? {
+            return if (value >= 10) this else null
+        }
+
+        override fun traverse(keep: Pair): List<SnailfishNumber> {
+            return listOf(this)
+        }
+
+        override fun magnitude(): Int {
+            return value
+        }
+
+        override fun toString(): String = value.toString()
+    }
+
+    class Pair(val left: SnailfishNumber, val right: SnailfishNumber) : SnailfishNumber() {
+
+        override fun replace(operations: Map<SnailfishNumber, SnailfishNumber>): SnailfishNumber {
+            operations[this]?.let { return it }
+            return Pair(left.replace(operations), right.replace(operations))
+        }
+
+        override fun findRegularNumberToSplit(): RegularNumber? {
+            left.findRegularNumberToSplit()?.let { return it }
+            right.findRegularNumberToSplit()?.let { return it }
+
+            return null
+        }
+
+        override fun traverse(keep: Pair): List<SnailfishNumber> {
+            return if (this == keep) listOf(this) else left.traverse(keep) + right.traverse(keep)
+        }
+
+        override fun magnitude(): Int {
+            return 3 * left.magnitude() + 2 * right.magnitude()
+        }
+
+        override fun toString(): String = "[$left,$right]"
+    }
+
+    private fun parseLine(line: String): SnailfishNumber {
+        var index = 0
+
+        fun recursiveParse(): SnailfishNumber {
+            // This is a Pair
+            if (line[index] == '[') {
+                index++
+                val left = recursiveParse()
+                index++ // ','
+                val right = recursiveParse()
+                index++ // ']'
+                return Pair(left, right)
+            }
+            // This is a regular number
+            return RegularNumber(line[index++].digitToInt())
+        }
+
+        return recursiveParse()
+    }
+
+    // addition
+    fun add(first: SnailfishNumber, second: SnailfishNumber): SnailfishNumber {
+        return Pair(first, second).reduce()
+    }
 
 }
+
---
+++ src/test/kotlin/me/nicolas/adventofcode/year2021/Day18Test.kt
@@ -0,0 +1,36 @@
+package me.nicolas.adventofcode.year2021
+
+internal class Day18Test {
+/*
+    @Test
+    fun magnitude() {
+        val result = Day18().magnitude("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
+        assertEquals(3488, result)
+    }
+
+    @Test
+    fun explode1() {
+        val result = Day18().explode("[[[[[9,8],1],2],3],4]")
+        assertEquals("[[[[0,9],2],3],4]", result)
+    }
+
+    @Test
+    fun explode2() {
+        val result = Day18().explode("[7,[6,[5,[4,[3,2]]]]]")
+        assertEquals("[7,[6,[5,[7,0]]]]", result)
+    }
+
+    @Test
+    fun explode3() {
+        val result = Day18().explode("[[6,[5,[4,[3,2]]]],1]")
+        assertEquals("[[6,[5,[7,0]]],3]", result)
+    }
+
+    @Test
+    fun explode4() {
+        val result = Day18().explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
+        assertEquals("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", result)
+    }
+
+ */
+}
