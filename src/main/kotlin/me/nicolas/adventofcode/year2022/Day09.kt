ebfab02cccbd287dd8e41b05f8db3e540a98acfd
Bulteau Nicolas <nicolas.bulteau@orange.com>
2022-12-09 10:16:36 +0100

    day 09 2022

---
+++ src/main/kotlin/me/nicolas/adventofcode/year2022/Day09.kt
@@ -0,0 +1,65 @@
+package me.nicolas.adventofcode.year2022
+
+import me.nicolas.adventofcode.AdventOfCodeDay
+import me.nicolas.adventofcode.prettyPrintPartOne
+import me.nicolas.adventofcode.prettyPrintPartTwo
+import me.nicolas.adventofcode.readFileDirectlyAsText
+import kotlin.math.abs
+
+fun main() {
+
+    val training = readFileDirectlyAsText("/year2022/day09/training.txt")
+    val data = readFileDirectlyAsText("/year2022/day09/data.txt")
+
+    val lines = data.split("\n")
+
+    val day = Day09("--- Day 9:  ---", "https://adventofcode.com/2022/day/9")
+    prettyPrintPartOne { day.solve(lines, 2) }
+    prettyPrintPartTwo { day.solve(lines, 10) }
+}
+
+private class Day09(title: String, adventOfCodeLink: String) : AdventOfCodeDay(title, adventOfCodeLink) {
+
+    private val motions = mapOf("R" to Pair(0, 1), "L" to Pair(0, -1), "D" to Pair(1, 0), "U" to Pair(-1, 0))
+
+    /**
+     * The head (H) and tail (T) must always be touching
+     * (diagonally adjacent and even overlapping both count as touching)
+     */
+    fun solve(seriesOfMotions: List<String>, nbKnots: Int): Int {
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        val rope = MutableList(nbKnots) { Pair(0, 0) }
+
+        seriesOfMotions
+            .map { it.split(" ") }
+            .forEach { (direction, steps) ->
+                repeat(steps.toInt()) {
+                    val motion = motions[direction]!!
+                    rope[0] = Pair(rope.first().first + motion.first, rope.first().second + motion.second) // Head
+                    (1 until nbKnots).forEach { index -> // Tail
+                        val knot1 = rope[index - 1]
+                        val knot2 = rope[index]
+                        // The knot1 and knot2 must always be touching
+                        // (diagonally adjacent and even overlapping both count as touching)
+                        val delta = Pair(abs(knot1.first - knot2.first), abs(knot1.second - knot2.second))
+                        if (delta.first == 2 || delta.second == 2) {
+                            val first = if (delta.first == 2) {
+                                (knot1.first + knot2.first) / 2
+                            } else {
+                                knot1.first
+                            }
+                            val second = if (delta.second == 2) {
+                                (knot1.second + knot2.second) / 2
+                            } else {
+                                knot1.second
+                            }
+                            rope[index] = Pair(first, second)
+                        }
+                    }
+                    visited.add(rope.last())
+                }
+            }
+
+        return visited.size
+    }
+}
