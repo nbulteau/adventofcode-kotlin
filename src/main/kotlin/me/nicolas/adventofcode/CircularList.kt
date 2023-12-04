1fb1c0d248696e8cb13a0520c5b8aa29001a307d
Nicolas Bulteau <nicolas.bulteau@orange.com>
2023-12-04 21:49:57 +0100

    feat: Day 02 year 2018

---
+++ src/main/kotlin/me/nicolas/adventofcode/CircularList.kt
@@ -0,0 +1,20 @@
+package me.nicolas.adventofcode
+
+class CircularList<out T>(private val list: List<T>) : List<T> by list {
+
+    override fun get(index: Int): T =
+        list[index.safely()]
+
+    override fun listIterator(index: Int): ListIterator<T> =
+        list.listIterator(index.safely())
+
+    override fun subList(fromIndex: Int, toIndex: Int): List<T> =
+        list.subList(fromIndex.safely(), toIndex.safely())
+
+    override fun toString(): String =
+        list.toString()
+
+    private fun Int.safely(): Int =
+        if (this < 0) (this % size + size) % size
+        else this % size
+}
--- src/main/kotlin/me/nicolas/adventofcode/year2018/Day01.kt
+++ src/main/kotlin/me/nicolas/adventofcode/year2018/Day01.kt
@@ -1,16 +1,41 @@
 package me.nicolas.adventofcode.year2018
 
-import me.nicolas.adventofcode.readFileDirectlyAsText
+import me.nicolas.adventofcode.*
 
 // https://adventofcode.com/2018/day/1
 fun main() {
 
-    val training = readFileDirectlyAsText("/year2018/day01/training.txt")
     val data = readFileDirectlyAsText("/year2018/day01/data.txt")
-
+    val lines = data.split("\n")
+    val day = Day01("--- Day 1: Chronal Calibration ---", "https://adventofcode.com/2018/day/1")
+    prettyPrintPartOne { day.partOne(lines) }
+    prettyPrintPartTwo { day.partTwo(lines) }
 }
 
-private class Day01 {
+class Day01(title: String, adventOfCodeLink: String) : AdventOfCodeDay(title, adventOfCodeLink) {
 
+    fun partOne(lines: List<String>): Int {
+
+        return lines.fold(0) { acc, line ->
+            acc + line.trim().toInt()
+        }
+    }
+
+    fun partTwo(lines: List<String>): Int {
+        val set = mutableSetOf<Int>()
+
+        val circularList: CircularList<Int> = CircularList(lines.map { it.trim().toInt() })
+        var index = 0
+        var acc = 0
+        do {
+            acc += circularList[index]
+            if (set.contains(acc)) {
+                return acc
+            } else {
+                set.add(acc)
+            }
+            index++
+        } while (true)
+    }
 }
 
