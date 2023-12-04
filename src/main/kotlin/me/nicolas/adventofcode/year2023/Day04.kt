c871a1fe684d3b4ad3e4a4b805dedd10373b609c
Nicolas Bulteau <nicolas.bulteau@orange.com>
2023-12-04 13:31:15 +0100

    feat: Day 04 year 2023

---
+++ src/main/kotlin/me/nicolas/adventofcode/year2023/Day04.kt
@@ -0,0 +1,66 @@
+package me.nicolas.adventofcode.year2023
+
+import me.nicolas.adventofcode.AdventOfCodeDay
+import me.nicolas.adventofcode.prettyPrintPartOne
+import me.nicolas.adventofcode.prettyPrintPartTwo
+import me.nicolas.adventofcode.readFileDirectlyAsText
+import java.util.regex.Pattern
+import kotlin.math.max
+import kotlin.math.pow
+
+fun main() {
+    val data = readFileDirectlyAsText("/year2023/day04/data.txt")
+    val lines = data.split("\n")
+    val day = Day04("--- Day 4: Scratchcards ---", "https://adventofcode.com/2023/day/4")
+    prettyPrintPartOne { day.partOne(lines) }
+    prettyPrintPartTwo { day.partTwo(lines) }
+}
+
+class Day04(title: String, adventOfCodeLink: String) : AdventOfCodeDay(title, adventOfCodeLink) {
+
+    private val spaces = Pattern.compile(" +")
+
+    data class Card(val winningNumbers: List<Int>, val numbersYouHave: List<Int>) {
+        val points: Int
+            // if you have 1 winning number, you win 1 point
+            // if winning is <= 0, you win 0 points thanks to the max(0, winning - 1)
+            get() = 2.0.pow(max(0, winning - 1)).toInt()
+
+        val winning: Int
+            get() = numbersYouHave.count { winningNumbers.contains(it) }
+    }
+
+    fun partOne(lines: List<String>): Int {
+        val cards = buildCards(lines)
+
+        return cards
+            .sumOf { card -> card.points }
+    }
+
+    fun partTwo(lines: List<String>): Int {
+        val cards = buildCards(lines)
+        val counts = IntArray(cards.size) { 1 }
+        cards.withIndex().forEach { (index, card) ->
+            for (i in 1..card.winning) {
+                if (index + i < cards.size) {
+                    counts[index + i] += counts[index]
+                }
+            }
+        }
+        return counts.sum()
+    }
+
+    private fun buildCards(lines: List<String>): List<Card> {
+        val cards = lines
+            .map { line ->
+                val numbers = line.split(":")[1]
+                val (leftPart, rightPart) = numbers.split("|")
+                val winningNumbers = leftPart.trim().split(spaces).map { it.toInt() }
+                val numbersYouHave = rightPart.trim().split(spaces).map { it.toInt() }
+
+                Card(winningNumbers, numbersYouHave)
+            }
+        return cards
+    }
+}
+
---
+++ src/test/kotlin/me/nicolas/adventofcode/year2023/Day04Test.kt
@@ -0,0 +1,31 @@
+package me.nicolas.adventofcode.year2023
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day04Test {
+
+    private val day = Day04("--- Day 4: Scratchcards ---", "https://adventofcode.com/2023/day/4")
+    private val data = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """.trimIndent()
+    @Test
+    fun `part one training`() {
+        val lines = data.split("\n")
+
+        assertEquals( 13, day.partOne(lines))
+    }
+
+    @Test
+    fun `part two training`() {
+        val lines = data.split("\n")
+
+        assertEquals( 30, day.partTwo(lines))
+    }
+}
