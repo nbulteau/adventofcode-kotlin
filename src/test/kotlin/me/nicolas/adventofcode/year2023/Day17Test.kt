54b0b343acc071fa1bc52a6cf440c4736c3dba52
Nicolas Bulteau <nicolas.bulteau@orange.com>
2023-12-17 15:24:15 +0100

    feat: day 17 2023

--- src/test/kotlin/me/nicolas/adventofcode/year2023/Day17Test.kt
+++ src/test/kotlin/me/nicolas/adventofcode/year2023/Day17Test.kt
@@ -1,23 +1,63 @@
 package me.nicolas.adventofcode.year2023
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 class Day17Test {
 
-    private val day = Day17(2023, 17, "")
-
-    private val data = """
-    """.trimIndent()
+    private val day = Day17(2023, 17, "Clumsy Crucible")
 
     @Test
     fun `part one training`() {
-        assertEquals(42, day.partOne(data))
+        val data = """
+            2413432311323
+            3215453535623
+            3255245654254
+            3446585845452
+            4546657867536
+            1438598798454
+            4457876987766
+            3637877979653
+            4654967986887
+            4564679986453
+            1224686865563
+            2546548887735
+            4322674655533
+        """.trimIndent()
+        assertEquals(102, day.partOne(data))
     }
 
     @Test
-    fun `part two training`() {
-        assertEquals(42, day.partTwo(data))
+    fun `part two training one`() {
+        val data = """
+            111111111111
+            999999999991
+            999999999991
+            999999999991
+            999999999991
+        """.trimIndent()
+
+        assertEquals(71, day.partTwo(data))
+    }
+
+    @Test
+    fun `part two training two`() {
+        val data = """
+            2413432311323
+            3215453535623
+            3255245654254
+            3446585845452
+            4546657867536
+            1438598798454
+            4457876987766
+            3637877979653
+            4654967986887
+            4564679986453
+            1224686865563
+            2546548887735
+            4322674655533
+        """.trimIndent()
+
+        assertEquals(94, day.partTwo(data))
     }
 }
