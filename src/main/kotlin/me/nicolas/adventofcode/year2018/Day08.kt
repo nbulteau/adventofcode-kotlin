ee1cd62c4c92176888f748bbaf69d528ce76fe00
Nicolas Bulteau <nicolas.bulteau@orange.com>
2023-12-27 18:10:58 +0100

    day 08 2018

--- src/main/kotlin/me/nicolas/adventofcode/year2018/Day08.kt
+++ src/main/kotlin/me/nicolas/adventofcode/year2018/Day08.kt
@@ -1,16 +1,60 @@
 package me.nicolas.adventofcode.year2018
 
 import me.nicolas.adventofcode.utils.AdventOfCodeDay
+import me.nicolas.adventofcode.utils.prettyPrintPartOne
+import me.nicolas.adventofcode.utils.prettyPrintPartTwo
 import me.nicolas.adventofcode.utils.readFileDirectlyAsText
 
 // https://adventofcode.com/2018/day/8
 fun main() {
-
-    val training = readFileDirectlyAsText("/year2018/day08/training.txt")
     val data = readFileDirectlyAsText("/year2018/day08/data.txt")
-
+    val day = Day08(2018, 8, "Memory Maneuver")
+    prettyPrintPartOne { day.partOne(data) }
+    prettyPrintPartTwo { day.partTwo(data) }
 }
 
-private class Day08 (year: Int, day: Int, title: String) : AdventOfCodeDay(year, day, title) {
+
+class Day08(year: Int, day: Int, title: String) : AdventOfCodeDay(year, day, title) {
+
+    // LicenseNode class represents a node in the license tree
+    private data class LicenseNode(val children: List<LicenseNode>, val meta: List<Int>) {
+        // Property to calculate the sum of metadata entries for a LicenseNode
+        val metaSum: Int
+            get() = meta.sum() + children.sumOf { it.metaSum }
+
+        // Property to calculate the value of a LicenseNode
+        val value: Int
+            get() = when {
+                children.isNotEmpty() -> meta.sumOf { children.getOrNull(it - 1)?.value ?: 0 }
+                else -> meta.sum()
+            }
+    }
+
+
+    fun partOne(data: String): Int {
+        val nodes = data.split(" ").map(String::toInt)
+        val (root, _) = listOfIntToLicences(nodes)
+
+        return root.metaSum
+    }
+
+    fun partTwo(data: String): Int {
+        val nodes = data.split(" ").map(String::toInt)
+        val (root, _) = listOfIntToLicences(nodes)
+
+        return root.value
+    }
+
+    private fun listOfIntToLicences(lists: List<Int>): Pair<LicenseNode, List<Int>> {
+        val (childCount, metaCount) = lists.take(2)
+        val children = mutableListOf<LicenseNode>()
+        val left = generateSequence(lists.drop(2)) {
+            val (nb, left) = listOfIntToLicences(it)
+            children += nb
+            left
+        }.take(childCount + 1).last()
+
+        return LicenseNode(children, left.take(metaCount)) to left.drop(metaCount)
+    }
 
 }
---
+++ src/test/kotlin/me/nicolas/adventofcode/year2018/Day08Test.kt
@@ -0,0 +1,23 @@
+package me.nicolas.adventofcode.year2018
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day08Test {
+
+    private val data = """
+        2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+    """.trimIndent()
+    private val day = Day08(2018, 8, "Memory Maneuver")
+
+    @Test
+    fun `part one training`() {
+        assertEquals(138, day.partOne(data))
+    }
+
+    @Test
+    fun `part two training`() {
+        assertEquals(66, day.partTwo(data))
+    }
+}
