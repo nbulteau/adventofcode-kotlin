1e5a8f716f3c7943faa9748b0be981a0cc059462
Nicolas Bulteau <nicolas.bulteau@orange.com>
2025-10-27 08:49:19 +0100

    chore: update dependencies and plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    id("com.github.ben-manes.versions") version "0.53.0"
+
+    kotlin("jvm") version "2.2.20"
     application
 }
 
@@ -11,13 +13,13 @@ repositories {
 }
 
 dependencies {
-    implementation("tools.aqua:z3-turnkey:4.13.0.1") // use Z3 to solve 2023 day24 part two
+    implementation("tools.aqua:z3-turnkey:4.14.1") // use Z3 to solve 2023 day24 part two
     implementation("net.sf.jung:jung-algorithms:2.1.1") // use JUNG to solve 2023 day25
     implementation("net.sf.jung:jung-graph-impl:2.1.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3") // to solve 2015 day 12
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0") // to solve 2015 day 12
 
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.assertj:assertj-core:3.27.6")
 }
 
 tasks.test {
