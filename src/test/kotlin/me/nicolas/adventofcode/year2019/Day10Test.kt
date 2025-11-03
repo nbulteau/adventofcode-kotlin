727e3186478bfb1f70e25de5f16dcaebbe0fcbec
nbulteau <nbulteau@gmail.com>
2025-11-03 10:45:25 +0100

    fix: update Day10Test to pass input directly for partOne and partTwo methods

--- src/test/kotlin/me/nicolas/adventofcode/year2019/Day10Test.kt
+++ src/test/kotlin/me/nicolas/adventofcode/year2019/Day10Test.kt
@@ -18,8 +18,8 @@ class Day10Test {
         """.trimIndent()
 
         println("\n=== Small Example Test ===")
-        val day10 = Day10(input)
-        val result = day10.partOne()
+        val day10 = Day10(2019, 10)
+        val result = day10.partOne(input)
         println("Result: $result")
         assertEquals(8, result, "Best location should detect 8 asteroids")
     }
@@ -39,8 +39,8 @@ class Day10Test {
             .#....####
         """.trimIndent()
 
-        val day10 = Day10(input)
-        val result = day10.partOne()
+        val day10 = Day10(2019, 10)
+        val result = day10.partOne(input)
         assertEquals(33, result, "Best location (5,8) should detect 33 asteroids")
     }
 
@@ -59,8 +59,8 @@ class Day10Test {
             .####.###.
         """.trimIndent()
 
-        val day10 = Day10(input)
-        val result = day10.partOne()
+        val day10 = Day10(2019, 10)
+        val result = day10.partOne(input)
         assertEquals(35, result, "Best location (1,2) should detect 35 asteroids")
     }
 
@@ -79,8 +79,8 @@ class Day10Test {
             .....#.#..
         """.trimIndent()
 
-        val day10 = Day10(input)
-        val result = day10.partOne()
+        val day10 = Day10(2019, 10)
+        val result = day10.partOne(input)
         assertEquals(41, result, "Best location (6,3) should detect 41 asteroids")
     }
 
@@ -110,8 +110,8 @@ class Day10Test {
         """.trimIndent()
 
         println("\n=== Largest Example Test (Expected 210) ===")
-        val day10 = Day10(input)
-        val result = day10.partOne()
+        val day10 = Day10(2019, 10)
+        val result = day10.partOne(input)
         println("Result: $result")
         assertEquals(210, result, "Best location (11,13) should detect 210 asteroids")
     }
@@ -142,8 +142,8 @@ class Day10Test {
         """.trimIndent()
 
         println("\n=== Part Two Largest Example Test ===")
-        val day10 = Day10(input)
-        val result = day10.partTwo()
+        val day10 = Day10(2019, 10)
+        val result = day10.partTwo(input)
         println("Result: $result")
         // The 200th asteroid to be vaporized is at 8,2 -> 8*100 + 2 = 802
         assertEquals(802, result, "200th vaporized asteroid should be at (8,2) -> 802")
