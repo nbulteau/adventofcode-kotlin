da318611eb87658c7510c3e4900429423ac71c61
Nicolas Bulteau <nicolas.bulteau@orange.com>
2025-12-07 16:07:19 +0100

    fix: fix unit test

--- src/test/kotlin/me/nicolas/adventofcode/year2019/Day04Test.kt
+++ src/test/kotlin/me/nicolas/adventofcode/year2019/Day04Test.kt
@@ -6,28 +6,29 @@ import org.junit.jupiter.api.Test
 
 internal class Day04Test {
 
+    private val day04 = Day04(2019, 4)
+
     @Test
     fun isIncreasingSequence() {
-
-        assertTrue(Day04().isIncreaseOrStayTheSame("111111"))
-        assertTrue(Day04().isIncreaseOrStayTheSame("123789"))
-        assertFalse(Day04().isIncreaseOrStayTheSame("223450"))
+        assertTrue(day04.isIncreaseOrStayTheSame("111111"))
+        assertTrue(day04.isIncreaseOrStayTheSame("123789"))
+        assertFalse(day04.isIncreaseOrStayTheSame("223450"))
     }
 
     @Test
     fun hasTwoAdjacentDigits() {
 
-        assertTrue(Day04().hasTwoAdjacentDigitsAreTheSame("111111"))
-        assertFalse(Day04().hasTwoAdjacentDigitsAreTheSame("123789"))
-        assertTrue(Day04().hasTwoAdjacentDigitsAreTheSame("223450"))
-        assertTrue(Day04().hasTwoAdjacentDigitsAreTheSame("123799"))
+        assertTrue(day04.hasTwoAdjacentDigitsAreTheSame("111111"))
+        assertFalse(day04.hasTwoAdjacentDigitsAreTheSame("123789"))
+        assertTrue(day04.hasTwoAdjacentDigitsAreTheSame("223450"))
+        assertTrue(day04.hasTwoAdjacentDigitsAreTheSame("123799"))
     }
 
     @Test
     fun hasAOnlyTwoAdjacentDigitsAreTheSame() {
 
-        assertTrue(Day04().hasAOnlyTwoAdjacentDigitsAreTheSame("112233"))
-        assertFalse(Day04().hasAOnlyTwoAdjacentDigitsAreTheSame("123444"))
-        assertTrue(Day04().hasAOnlyTwoAdjacentDigitsAreTheSame("111122"))
+        assertTrue(day04.hasAOnlyTwoAdjacentDigitsAreTheSame("112233"))
+        assertFalse(day04.hasAOnlyTwoAdjacentDigitsAreTheSame("123444"))
+        assertTrue(day04.hasAOnlyTwoAdjacentDigitsAreTheSame("111122"))
     }
 }
